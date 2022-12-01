ab32517c9568abd214ba66e0774cc33f5e846821
Tobias Gradl <tobias@tgradl.de>
2022-12-01 21:35:30 +0100

    Continue after false upload failure to set name and type

--- garmin_uploader/workflow.py
+++ garmin_uploader/workflow.py
@@ -81,7 +81,8 @@ class Activity(object):
             self.id, uploaded = api.upload_activity(user.session, self)
         except GarminAPIException as e:
             logger.warning('Upload failure: {}'.format(e))
-            return False
+            
+        uploaded = True
 
         if uploaded:
             logger.info('Uploaded activity {}'.format(self))
